19696e52cd64a8ae8b6e32f85633bb504a77c9b6
mithunsen04 <mithunsen559@gmail.com>
2023-12-21 17:12:05 +0530

    listing page reservation favourit page done

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,49 @@
-export default function Home() {
-  return <div className="text-rose-500 text-2xl">Hello Airbnb</div>;
-}
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import getListings from "@/app/actions/getListings";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import ListingCard from "./components/listings/ListingCard";
+
+const Home = async () => {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <Container>
+        <div
+          className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+        >
+          {listings.map((listing: any) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          ))}
+        </div>
+      </Container>
+    </ClientOnly>
+  );
+};
+
+export default Home;
